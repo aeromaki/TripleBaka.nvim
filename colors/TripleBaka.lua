c07e1a7fe24cba356d6db8e614311d90c3fec760
-3σ <yurippe@m3sigma.net>
2025-03-22 06:16:39 +0900

    -

--- colors/TripleBaka.lua
+++ colors/TripleBaka.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
 vim.opt.background = 'light'
-vim.g.colors_name = 'triple_baka'
+vim.g.colors_name = 'TripleBaka'
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
@@ -14,8 +14,8 @@ vim.g.colors_name = 'triple_baka'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.triple_baka'] = nil
+package.loaded['lush_theme.TripleBaka'] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.triple_baka'))
+require('lush')(require('lush_theme.TripleBaka'))
 
